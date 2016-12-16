ceef511ad6389120b608574c5d1a79a06c69646f
Brian Roper <broper138@gmail.com>
2016-12-15 23:35:29 -0500

    3.1.1
    feat: MVP implementation
    -created packages, view and presenters
    -added View interface classes for existing views

---
+++ Application/src/main/java/com/ambercam/android/camera2basic/presenters/DetailPresenter.java
@@ -0,0 +1,34 @@
+package com.ambercam.android.camera2basic.presenters;
+
+import com.ambercam.android.camera2basic.views.DetailView;
+
+/**
+ * Created by brianroper on 12/15/16.
+ */
+
+public class DetailPresenter implements Presenter<DetailView> {
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onStop() {
+
+    }
+
+    @Override
+    public void onPause() {
+
+    }
+
+    @Override
+    public void attachView(DetailView view) {
+
+    }
+}
